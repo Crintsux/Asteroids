7c8cac6411c0be84e971f46b04dcdabe670ec2c1
Crintsux <fifa.d@seznam.cz>
2025-02-04 23:49:38 +0100

    player class added and player object initialized.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # Console start message.
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    # Pygame initializes, game screen sets up, player gets initialized.
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # MAIN GAME LOOP
     while 1:
+
+        # Allows us to quit the game.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+            
+        # Make the screen black and move the frame. clock.tick(60) limits the fps to 60
+        # and saves delta time into dt variable.
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
